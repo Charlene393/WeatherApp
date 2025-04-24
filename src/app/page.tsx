d58c97348793f8e7b1edc09a69b35280d6759715
Charlene393 <charlenembugua0@gmail.com>
2025-04-24 09:03:53 +0300

    Testing Weather API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Navbar from "@/components/Navbar";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
 import Image from "next/image";
 
 
@@ -92,18 +93,37 @@ export default function Home() {
     queryKey: ["repoData"],
     queryFn: async () => {
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=Nairobi&appid={process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=40`
+        `https://api.openweathermap.org/data/2.5/forecast?q=Nairobi&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=40`
       );
       return data;
-    },
+    },    
   });
   
+  const firstData = data?.list[0];
   console.log("data", data);
 
-  if (isPending) return 'Loading...'
+  if (isPending) return(
+    <div className="flex items-center min-h-screen justify-center"> 
+    <p className="animate-bounce">Loading...</p>
+    </div>
+  );
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/*today data*/}
+        <section></section>
+        <div className="">
+          <h2 className="flex gap-1 text-2x1 items-end">
+            <p>{format(parseISO(firstData?.dt_txt ??''),'EEEE')}</p>
+            <p>{format(parseISO(firstData?.dt_txt ??''),'dd/MM/yyyy')}</p>
+          </h2>
+          <div></div>
+
+        </div>
+        {/* 7 day forecast data*/}
+        <section></section>
+      </main>
     </div>
   );
 }

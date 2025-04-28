00f1ca1c7374e67ac61824d7be5c945b5332456c
Charlene393 <charlenembugua0@gmail.com>
2025-04-28 18:54:47 +0300

    date edit

--- src/components/ForecastWeatherDetails.tsx
+++ src/components/ForecastWeatherDetails.tsx
@@ -1,10 +1,55 @@
 import React from "react";
 import Container from "./Container";
+import WeatherIcon from "./WeatherIcon";
+import WeatherDetails, { WeatherDetailsProps } from "./WeatherDeatils";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
 
-type props ={}
+export interface ForecastWeatherDetailsProps  extends WeatherDetailsProps{
+    weatherIcon: string;
+    date: string;
+    day: string;
+    temp:number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    description: string;
+}
 
-export default function ForecastWeatherDetails({}: props) {
+export default function ForecastWeatherDetails(props: ForecastWeatherDetailsProps) {
+    const{
+        weatherIcon = "01d",
+        date = "2023-10-01",
+        day = "Monday", 
+        temp = 0, 
+        feels_like,
+        temp_min ,
+        temp_max ,
+        description,
+    } = props
     return(
-        <Container></Container>
+        <Container className="gap-4">
+            {/*left*/}
+            <section className="flex gap-4 items-center px-4">
+                <div>
+                    <WeatherIcon iconname={weatherIcon}/>
+                    <p>{date}</p>
+                    <p className="text-sm">{day}</p>
+                </div>
+
+                <div className="flex flex-col px-4">
+                    <span className="text-5xl">{convertKelvinToCelsius(temp?? 0)}° </span>
+                    <p className="text-xs space-x-1 whitespace-nowrap">
+                        <span>Feels like</span>
+                        <span>{convertKelvinToCelsius(feels_like ?? 0)}°</span>
+                    </p>
+                    <p className="capitalize">{description}</p>
+
+                </div>
+            </section>
+
+            <section className="overflow-x-auto flex justify-between gap-4 px-4 w-full pr-10">
+                <WeatherDetails {...props}/>
+            </section>
+        </Container>
     )
 }
